c0bc15594490f3f8e243e54b55cb6c645e7549ac
Matt Kretschmer <matthewk@bbd.co.za>
2023-01-18 13:05:00 +0200

    testing fix

--- successjs.js
+++ successjs.js
@@ -1,30 +1,30 @@
-// function GetURLParameter()
-// {
-//     console.log("The current token is:")
-//     var sPageURL = window.location.search.substring(1);
-//     var sURLVariables = sPageURL.split('&');
-//     for (var i = 0; i < sURLVariables.length; i++)
-//     {
-//         var sParameterName = sURLVariables[i].split('=');
-//         if (sParameterName[0] == "code") 
-//         {
-//             var codeParam = sParameterName[1];
-//         }
-//     }
+function GetURLParameter()
+{
+    console.log("The current token is:")
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++)
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == "code") 
+        {
+            var codeParam = sParameterName[1];
+        }
+    }
 
-//     fetch('https://github.com/login/oauth/access_token', {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(
-//         {   "client_id": "f0b6b732e7117e532a2a",
-//             "client_secret": "84cd58be0d824e5069311a82f4bc8ca50be1ab58",
-//             "code": codeParam
-//         })
-//     })
-//     .then(response => response.json())
-//     .then(response => console.log(JSON.stringify(response)))
-// }
-// document.getElementById("tokenBtn").onclick = GetURLParameter;
+    fetch('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+        {   "client_id": "f0b6b732e7117e532a2a",
+            "client_secret": "84cd58be0d824e5069311a82f4bc8ca50be1ab58",
+            "code": codeParam
+        })
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
+}
+document.getElementById("tokenBtn").onclick = GetURLParameter;
